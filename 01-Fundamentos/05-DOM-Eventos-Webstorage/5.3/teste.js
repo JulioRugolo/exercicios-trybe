400a63dc310aa2b0a6b1bea04ec0f3ee258073c0
Julio Rugoloe <juliorugolo@gmail.com>
2022-12-12 20:05:12 -0300

    aula 5.2

---
+++ 01-Fundamentos/05-DOM-Eventos-Webstorage/5.3/teste.js
@@ -0,0 +1,10 @@
+let a = "Serei a melhor pessoa dev";
+let b = "Esse é meu jeito dev de ser!";
+let num = 2;
+let strings = [a, b];
+
+console.log((typeof strings) == 'array');
+console.log((typeof strings.length) == (typeof num));
+console.log((typeof num) == (typeof 'number'));
+console.log((typeof b) == a);
+console.log((typeof a) == (typeof num));
