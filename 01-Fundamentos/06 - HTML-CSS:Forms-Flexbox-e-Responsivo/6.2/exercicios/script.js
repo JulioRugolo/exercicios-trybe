2e76a7a8ee3cf9010370b407ebc6b86a226acba2
juliorugolo <juliorugolo@gmail.com>
2023-01-13 14:48:19 -0300

    exercicios 6.3

--- 01-Fundamentos/06 - HTML-CSS:Forms-Flexbox-e-Responsivo/6.2/exercicios/script.js
+++ 01-Fundamentos/06 - HTML-CSS:Forms-Flexbox-e-Responsivo/6.2/exercicios/script.js
@@ -1,8 +1,32 @@
+
 const submitButton = document.getElementById('submit-button');
 const submitFoto = document.getElementById('input-foto');
 const maxInputAbout = document.getElementById('input-about').value.length;
 const maxInputEmail = document.getElementById('input-email').value.length;
 const maxInputNome = document.getElementById('input-nome').value.length;
+const inputNome = document.getElementById('input-nome');
+const inputEmail = document.getElementById('input-email');
+const inputSobrenome = document.getElementById('sobrenome');
+const picker2 = new Pikaday({ field: document.getElementById('input-date') });
+let nome = '';
+let email = '';
+let sobrenome = '';
+
+inputEmail.addEventListener('input', () => {
+    email = inputEmail.value
+})
+
+inputSobrenome.addEventListener('input', () => {
+    sobrenome = inputSobrenome.value
+})
+
+function valiteInputs() {
+    if (nome.length > 0 && email.length > 0 && sobrenome.length > 0) {
+        submitButton.disabled = false
+    }
+}
+
+document.body.addEventListener('click', valiteInputs)
 
 function validadeTerms() {
     if (submitFoto.checked === true) {
@@ -15,7 +39,7 @@ function validadeTerms() {
 submitButton.addEventListener('click', (event) => {
     if (maxInputAbout < 500 && maxInputEmail < 50 && maxInputNome < 40 ) {
         if (submitFoto.checked === true) {
-            alert('Dados enviados com sucesso! Obrigado pro participar do concurso TrybeTrip')
+            alert(`Obrigado ${nome}, por participar do concurso TrybeTrip! Dados enviados com sucesso!`)
             
         } else if (submitFoto.checked === false){
             event.preventDefault();
@@ -26,3 +50,21 @@ submitButton.addEventListener('click', (event) => {
         alert('Dados Inválidos');
     }
 });
+
+const picker = new Pikaday({
+    field: document.getElementById('date'),
+    format: 'D/M/YYYY',
+    toString(date, format) {
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    },
+    parse(dateString, format) {
+        const parts = dateString.split('/');
+        const day = parseInt(parts[0], 10);
+        const month = parseInt(parts[1], 10) - 1;
+        const year = parseInt(parts[2], 10);
+        return new Date(year, month, day);
+    }
+  });
